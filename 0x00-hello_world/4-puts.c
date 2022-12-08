f9ee7823d904d320caac1efa3eb99ef38e1638bb
Ona Amali <dvdamali@gmail.com>
2022-12-08 13:28:08 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
-
-
 /**
+ *main - Entry point
  *
- *  * main - Entry point
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
 	return (0);
 }
