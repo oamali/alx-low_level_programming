69abbab9410f90e8808645054afaa67432fe9da5
Ona Amali <dvdamali@gmail.com>
2022-12-08 15:12:36 -0800

    6-size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 /**
- *
- * main - Entry point
- *
- *
- *
- * Return: Always 0 (Success)
- *
- */
+*
+* main - Entry point
+*
+* Description - 'This program prints using printf to print a string of text'
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 
 }
