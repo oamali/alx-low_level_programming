3d54c09dc6c53b8c4b11c2218a44842d787c2f37
Ona Amali <dvdamali@gmail.com>
2022-12-10 00:36:09 +0100

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,26 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * main - initiating betty.
  *
- * Description: print last diit in random number
+ * Description: main
  *
- * Return: Always 0 (Success)
- *
+ * Return: always 0 (Success)
  */
 int main(void)
 {
-    int n, last;
+        int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    last = n % 10;
-    if (last > 5)
-        printf(“Last digit of %d is %d and is greater than 5\n”, n, last);
-    else if (last == 0)
-        printf(“Last digit of %d is %d and is 0\n”, n, last);
-    else if (last < 6 && last != 0)
-        printf(“Last digit of %d is %d and is less than 6 and not 0\n”, n, last);
-    return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+        else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
 }
