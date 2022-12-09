822cdccec3cfb9b9963b202c876903b368fcbc8e
Ona Amali <dvdamali@gmail.com>
2022-12-09 09:13:37 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - print quote
  *
@@ -7,8 +8,9 @@
  *
  * Return:Always 1 (Success)
  */
+
 int main(void)
 {
-	write(2, "and that piece os art is useful\" - Dora Korpar, 2015 - 10 - 19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n", 59);
 	return (1);
 }
